dab45e7890e52eb91b68bdedafe5e6c042fb79a8
Nerogar <nerogar@arcor.de>
2015-05-24 00:26:00 +0200

    newline at file endings

--- src/de/nerogar/noise/render/IRenderTarget.java
+++ src/de/nerogar/noise/render/IRenderTarget.java
@@ -2,9 +2,20 @@ package de.nerogar.noise.render;
 
 public interface IRenderTarget {
 
-	/**
-	 * Binds this target. All OpenGL calls will draw to this target now. 
-	 */
+	/**Binds this target. All OpenGL calls will draw to this target after calling this method.*/
 	public void bind();
+
+	/**
+	 * Sets the resolution of this render targe
+	 * @param width the new width
+	 * @param height the new height
+	 */
+	public void setResolution(int width, int height);
+
+	/**@return the width of this render target*/
+	public int getWidth();
+
+	/**@return the height of this render target*/
+	public int getHeight();
 
 }
